6c3910c7af0176260021ed00958c86b7a406e1f2
AndrewAi <andrewiriwin@gmail.com>
2016-04-22 21:36:09 +0100

    added input box & add task button, but blank items being added

--- sauceProject/www/js/controllers.js
+++ sauceProject/www/js/controllers.js
@@ -1,9 +1,30 @@
 angular.module('starter.controllers', [])
 
-.controller('ShortTermCtrl', function($scope) {})
+.controller('ShortTermCtrl', function($scope, Task) {
 
-.controller('MediumTermCtrl', function($scope){})
+  $scope.data = Task.data;
+
+
+  var inputData = {
+    newTask: ""
+  };
+
+
+  function addTask(){
+
+    Task.addTask(inputData.newTask);
+    inputData.newTask = "";
+  }
+
+
+  $scope.inputData = inputData;
+  $scope.addTask = addTask;
+
+
+})
+
+.controller('MediumTermCtrl', function($scope, Task){})
 
 
 
-.controller('LongTermCtrl', function($scope) {});
+.controller('LongTermCtrl', function($scope, Task) {});
--- sauceProject/www/js/services.js
+++ sauceProject/www/js/services.js
@@ -1,3 +1,25 @@
 angular.module('starter.services', [])
 
 
+.factory('Task', function(){
+
+  var data = {
+    tasks: [
+      {title: "Boom", added: new Date()},
+      {title: "Dooom", added: new Date()}
+    ]
+  };
+
+
+  function addTask(title){
+    data.tasks.push({title: title, added: new Date()});
+  }
+
+
+
+  return{
+    data: data
+    , addTask: addTask
+  };
+
+});
